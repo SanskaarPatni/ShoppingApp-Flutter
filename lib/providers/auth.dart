cd4b09e26b209452ba75a27907e1ca304c300d72
Sanskaar Patni <patnisanskaar@gmail.com>
2020-04-03 23:58:11 +0530

    Update auth.dart

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -30,7 +30,7 @@ class Auth with ChangeNotifier {
   Future<void> _authenticate(
       String email, String password, String urlSegment) async {
     final url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key= AIzaSyDyMfmkOIeSFIh4v2U-HtHIfv-WJbzZQes';
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key= API_KEY';
     try {
       final response = await http.post(
         url,
